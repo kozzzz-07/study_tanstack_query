e6af2fdba06e3091eb79869ec28c99381407d6ab
kozzzz-07 <kozzzz-07@users.noreply.github.com>
2026-01-05 22:30:15 +0900

    フォロー、アンフォロー

--- src/api/git-hub.ts
+++ src/api/git-hub.ts
@@ -21,3 +21,65 @@ export const searchGithubUser = async (query: string) => {
   console.log(data);
   return data.items;
 };
+
+// https://docs.github.com/en/rest/users/followers?apiVersion=2022-11-28#check-if-a-person-is-followed-by-the-authenticated-user
+export const checkIfFollowingUser = async (username: string) => {
+  const res = await fetch(
+    `${import.meta.env.VITE_GITHUB_API_URL}/user/following/${username}`,
+    {
+      headers: {
+        Authorization: `Bearer ${import.meta.env.VITE_GITHUB_API_TOKEN}`,
+        Accept: "application/vnd.github+json",
+      },
+    }
+  );
+
+  if (res.status === 204) {
+    return true;
+  } else if (res.status === 404) {
+    return false;
+  } else {
+    const errorData = await res.json().catch(() => null);
+    throw new Error(errorData.message || "Failed to check follow status");
+  }
+};
+
+export const followGithubUser = async (username: string) => {
+  const res = await fetch(
+    `${import.meta.env.VITE_GITHUB_API_URL}/user/following/${username}`,
+    {
+      method: "PUT",
+      headers: {
+        Authorization: `Bearer ${import.meta.env.VITE_GITHUB_API_TOKEN}`,
+        Accept: "application/vnd.github+json",
+        "Content-Type": "application/json",
+      },
+    }
+  );
+
+  if (!res.ok) {
+    const errorData = await res.json();
+    throw new Error(errorData.message || "Failed to follow user");
+  }
+  return true;
+};
+
+export const unfollowGithubUser = async (username: string) => {
+  const res = await fetch(
+    `${import.meta.env.VITE_GITHUB_API_URL}/user/following/${username}`,
+    {
+      method: "DELETE",
+      headers: {
+        Authorization: `Bearer ${import.meta.env.VITE_GITHUB_API_TOKEN}`,
+        Accept: "application/vnd.github+json",
+        "Content-Type": "application/json",
+      },
+    }
+  );
+
+  if (!res.ok) {
+    const errorData = await res.json();
+    throw new Error(errorData.message || "Failed to unfollow user");
+  }
+  return true;
+};
--- src/components/UserCard/index.tsx
+++ src/components/UserCard/index.tsx
@@ -1,22 +1,84 @@
+import { useMutation, useQuery } from "@tanstack/react-query";
 import type { GitHubUser } from "../../types";
 import "./UserCard.css";
-import { FaGithubAlt } from "react-icons/fa";
+import { FaGithubAlt, FaUserMinus, FaUserPlus } from "react-icons/fa";
+import {
+  checkIfFollowingUser,
+  followGithubUser,
+  unfollowGithubUser,
+} from "../../api/git-hub";
 
 export function UserCard({ user }: { user: GitHubUser }) {
+  const { data: isFollowing, refetch } = useQuery({
+    queryKey: ["follow-status", user.login],
+    queryFn: () => checkIfFollowingUser(user.login),
+    enabled: !!user.login,
+  });
+
+  const followMutation = useMutation({
+    mutationFn: () => followGithubUser(user.login),
+    onSuccess: () => {
+      console.log(`You are now following ${user.login}`);
+      refetch();
+    },
+    onError: (err) => {
+      console.error(err.message);
+    },
+  });
+
+  const unfollowMutation = useMutation({
+    mutationFn: () => unfollowGithubUser(user.login),
+    onSuccess: () => {
+      console.log(`You are no longer following ${user.login}`);
+      refetch();
+    },
+    onError: (err) => {
+      console.error(err.message);
+    },
+  });
+
+  const handleFollow = () => {
+    if (isFollowing) {
+      unfollowMutation.mutate();
+    } else {
+      followMutation.mutate();
+    }
+  };
+
   return (
     <div className="user-card">
       <img src={user.avatar_url} alt={user.name} className="avatar" />
       <h2>{user.name || user.login}</h2>
       <div className="bio">{user.bio}</div>
-      <a
-        href={user.html_url}
-        className="profile-btn"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <FaGithubAlt />
-        View GitGub Profile
-      </a>
+      <div className="user-card-buttons">
+        {!!import.meta.env.VITE_GITHUB_API_TOKEN && (
+          <button
+            onClick={handleFollow}
+            className={`follow-btn ${isFollowing ? "following" : ""}`}
+            disabled={followMutation.isPending || unfollowMutation.isPending}
+          >
+            {isFollowing ? (
+              <>
+                <FaUserMinus className="follow-icon" /> Following
+              </>
+            ) : (
+              <>
+                <FaUserPlus className="follow-icon" /> Follow User
+              </>
+            )}
+          </button>
+        )}
+
+        <a
+          href={user.html_url}
+          className="profile-btn"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaGithubAlt />
+          View GitGub Profile
+        </a>
+      </div>
     </div>
   );
 }
