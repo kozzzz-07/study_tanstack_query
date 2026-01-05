18cb16d4c5486916bd7272193a2fd54e4b0be118
kozzzz-07 <kozzzz-07@users.noreply.github.com>
2026-01-05 12:36:08 +0900

    プリフェチ

--- src/components/RecentSearches/index.tsx
+++ src/components/RecentSearches/index.tsx
@@ -1,5 +1,7 @@
+import { useQueryClient } from "@tanstack/react-query";
 import "./RecentSearches.css";
 import { FaClock, FaUser } from "react-icons/fa";
+import { fetchGithubUser } from "../../api/git-hub";
 
 type Props = {
   users: string[];
@@ -7,6 +9,8 @@ type Props = {
 };
 
 export function RecentSearches({ users, onSelect }: Props) {
+  const queryClient = useQueryClient();
+
   return (
     <div className="recent-searches">
       <div className="recent-header">
@@ -16,7 +20,15 @@ export function RecentSearches({ users, onSelect }: Props) {
       <ul>
         {users.map((user) => (
           <li key={user}>
-            <button onClick={() => onSelect(user)}>
+            <button
+              onClick={() => onSelect(user)}
+              onMouseEnter={() => {
+                queryClient.prefetchQuery({
+                  queryKey: ["users", user],
+                  queryFn: () => fetchGithubUser(user),
+                });
+              }}
+            >
               <FaUser className="user-icon" />
               {user}
             </button>
